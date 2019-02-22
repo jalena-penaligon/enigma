231aef6195be7ebced067a61356a3fe82f6fde66
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-22 16:49:35 -0700

    Added method to create a random key

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -6,13 +6,19 @@ class Enigma
   def initialize
   end
 
-  def encrypt(message, key, date)
+  def encrypt(message, key = nil, date = nil)
+    key = create_key if key == nil
+    date = todays_date if date == nil
     encryption = Encryption.run(message, key, date)
     {
       encryption: encryption,
       key: key,
       date: date
     }
+  end
+
+  def create_key
+    rand.to_s[2..6]
   end
 
 end
