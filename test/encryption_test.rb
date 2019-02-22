cc3fc66547cdfd9efe04e0d9b3b06efe334d3fa4
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-22 10:27:08 -0700

    Creates test for structure of encryption input and output

--- test/encryption_test.rb
+++ test/encryption_test.rb
@@ -3,9 +3,21 @@ require './test/test_helper'
 class EncryptionTest < Minitest::Test
 
   def test_it_exists
-    encryption = Encryption.new
+    encryption = Encryption.new("hello world end")
 
-    assert_instance_pf Encryption, encryption
+    assert_instance_of Encryption, encryption
+  end
+
+  def test_it_has_a_message
+    encryption = Encryption.new("hello world end")
+
+    assert_equal "hello world end", encryption.message
+  end
+
+  def test_encrypted_starts_as_an_empty_string
+    encryption = Encryption.new("hello world end")
+
+    assert_equal " ", encryption.encrypted
   end
 
 end
