a9ff8233750aec7547e67b7cebd40404254c7c66
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-23 12:21:51 -0700

    Added functionality to find the index of each letter

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -13,7 +13,29 @@ class Encryption
 
   def self.run(message, key, date)
     encryption = Encryption.new(message, key, date)
+    encryption.shift_message
     encryption.message
+  end
+
+  def shift_message
+    alphabet = ("a".."z").to_a << " "
+
+    shift_by = shift_values
+
+    split_message = @message.split(//).to_enum
+    message_and_index = []
+    split_message.with_index { |letter,index| message_and_index << [letter, index] }
+    binding.pry
+
+    split_message.map do |letter|
+      shift_by.map do |shift|
+        index = alphabet.index(letter)
+        rotate_by = index + shift
+        alphabet.rotate(rotate_by).first
+      end
+    end
+
+    binding.pry
   end
 
 end
