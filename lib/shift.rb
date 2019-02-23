4485ee45d3879be6d6936324a95991b7f1a573c6
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-22 17:29:00 -0700

    Updated to return Arrays of Integers

--- lib/shift.rb
+++ lib/shift.rb
@@ -6,7 +6,15 @@ module Shift
     b_key = keys[1] + keys[2]
     c_key = keys[2] + keys[3]
     d_key = keys[3] + keys[4]
-    [a_key, b_key, c_key, d_key]
+    [a_key.to_i, b_key.to_i, c_key.to_i, d_key.to_i]
+  end
+
+  def offset_values
+    squared = (@date.to_i ** 2).to_s
+    last_four = squared.split(//).last(4)
+    last_four.map do |num|
+      num.to_i
+    end 
   end
 
 end
