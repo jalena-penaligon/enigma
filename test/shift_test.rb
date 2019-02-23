1bd7bb028535bab27da5472b9b2fea6f6d387f7b
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-22 17:28:49 -0700

    Updated arrays to include integers

--- test/shift_test.rb
+++ test/shift_test.rb
@@ -5,12 +5,19 @@ class ShiftTest < Minitest::Test
   def test_split_key_into_array
     encryption = Encryption.new("hello world end", "08304", "291018")
 
-    assert_equal ["08", "83", "30", "04"], encryption.key_values
+    assert_equal [8, 83, 30, 04], encryption.key_values
   end
 
   def test_find_offsets_from_date
     encryption = Encryption.new("hello world end", "08304", "291018")
 
-    assert_equal ["6", "3", "2", "4"], encryption.offset_values
+    assert_equal [6, 3, 2, 4], encryption.offset_values
+  end
+
+  def test_zip_keys_and_offsets
+    skip
+    encryption = Encryption.new("hello world end", "08304", "291018")
+
+    assert_equal [[8, 6], [83, 3], [30, 2], [4, 4]]
   end
 end
