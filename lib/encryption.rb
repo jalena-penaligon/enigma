c5a8ebf7d8f4a4a48d187f83c031046c5ca35523
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-24 08:51:56 -0700

    Refactored to simplify index and shift values

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -20,18 +20,23 @@ class Encryption
   def find_letter_and_index
     split_message = @message.split(//).to_enum
     message_and_index = []
-    split_message.with_index { |letter,index| message_and_index << [letter, index += 1] }
+    split_message.with_index { |letter,index| message_and_index << [letter, index] }
     message_and_index
   end
 
   def assign_shift_index
     shift_index = find_letter_and_index
     shift_index.each do |letter_and_index|
-      if letter_and_index[1] > 4 && letter_and_index[1] % 4 != 0
+      if letter_and_index[1] > 3
         letter_and_index[1] = letter_and_index[1] % 4
-      elsif letter_and_index[1] > 4 && letter_and_index[1] % 4 == 0
-        letter_and_index[1] = 4
       end
+    end
+  end
+
+  def assign_shift_value
+    shift_by = shift_values
+    shift_value = assign_shift_index.each do |letter_and_index|
+      letter_and_index[1] = shift_by[letter_and_index[1]]
     end
   end
 
