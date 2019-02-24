0cd793a7b1bbdbc7f59668043bd672e09dd8d220
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-23 17:56:24 -0700

    Passed test to combine letter and index

--- lib/encryption.rb
+++ lib/encryption.rb
@@ -17,25 +17,19 @@ class Encryption
     encryption.message
   end
 
+  def find_letter_and_index
+    split_message = @message.split(//).to_enum
+    message_and_index = []
+    split_message.with_index { |letter,index| message_and_index << [letter, index += 1] }
+    message_and_index
+  end
+
   def shift_message
     alphabet = ("a".."z").to_a << " "
 
     shift_by = shift_values
 
-    split_message = @message.split(//).to_enum
-    message_and_index = []
-    split_message.with_index { |letter,index| message_and_index << [letter, index] }
-    binding.pry
-
-    split_message.map do |letter|
-      shift_by.map do |shift|
-        index = alphabet.index(letter)
-        rotate_by = index + shift
-        alphabet.rotate(rotate_by).first
-      end
-    end
-
-    binding.pry
+    # binding.pry
   end
 
 end
