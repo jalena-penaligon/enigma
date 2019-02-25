0b7bc2c826f33acc2ddbbce24cc8fcaad0a7d6d2
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 09:20:23 -0700

    Added test to assign shift values

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -41,4 +41,23 @@ class EncryptionTest < Minitest::Test
     assert_equal expected, decryption.find_letter_and_index
   end
 
+  def test_assign_index_value_between_0_and_3
+    decryption = Decryption.new("keder ohulw", "02715", "040895")
+    expected = [["k", 0], ["e", 1], ["d",2],
+                ["e", 3], ["r", 0], [" ", 1],
+                ["o", 2], ["h", 3], ["u", 0],
+                ["l", 1], ["w", 2]]
+    assert_equal expected, decryption.assign_shift_index
+  end
+
+  def test_assign_shift_value
+    decryption = Decryption.new("keder ohulw", "02715", "040895")
+
+    expected = [["k", 3], ["e", 27], ["d",73],
+                ["e", 20], ["r", 3], [" ", 27],
+                ["o", 73], ["h", 20], ["u", 3],
+                ["l", 27], ["w", 73]]
+    assert_equal expected, encryption.assign_shift_value
+  end
+
 end
