abf15207c3ace17a9842cd8be4c3fb3416a5c732
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 11:32:24 -0700

    Removed redundant tests

--- test/decryption_test.rb
+++ test/decryption_test.rb
@@ -32,43 +32,6 @@ class DecryptionTest < Minitest::Test
     assert_equal String, decryption.class
   end
 
-  def test_print_message_letters_and_index
-    decryption = Decryption.new("keder ohulw", "02715", "040895")
-    expected = [["k", 0], ["e", 1], ["d",2],
-                ["e", 3], ["r", 4], [" ", 5],
-                ["o", 6], ["h", 7], ["u", 8],
-                ["l", 9], ["w", 10]]
-    assert_equal expected, decryption.find_letter_and_index
-  end
-
-  def test_assign_index_value_between_0_and_3
-    decryption = Decryption.new("keder ohulw", "02715", "040895")
-    expected = [["k", 0], ["e", 1], ["d",2],
-                ["e", 3], ["r", 0], [" ", 1],
-                ["o", 2], ["h", 3], ["u", 0],
-                ["l", 1], ["w", 2]]
-    assert_equal expected, decryption.assign_shift_index
-  end
-
-  def test_assign_shift_value
-    decryption = Decryption.new("keder ohulw", "02715", "040895")
-
-    expected = [["k", 3], ["e", 27], ["d",73],
-                ["e", 20], ["r", 3], [" ", 27],
-                ["o", 73], ["h", 20], ["u", 3],
-                ["l", 27], ["w", 73]]
-    assert_equal expected, decryption.assign_shift_value
-  end
-
-  def test_find_letter_shift_value
-    decryption = Decryption.new("keder ohulw", "02715", "040895")
-    expected = [[10, 3], [4, 27], [3, 73],
-                [4, 20], [17, 3], [26, 27],
-                [14, 73], [7, 20], [20, 3],
-                [11, 27], [22, 73]]
-    assert_equal expected, decryption.assign_letter_shift
-  end
-
   def test_message_can_be_shifted
     decryption = Decryption.new("keder ohulw", "02715", "040895")
 
