e0490bf86fc8d029d39f27e335febc6d2ca5b2f4
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 08:28:53 -0700

    Passed test for self.run to return a string

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -2,10 +2,15 @@ class Decryption
   attr_reader :key, :date
   attr_accessor :message
 
-def initialize(message, key, date)
-  @message = message
-  @key = key
-  @date = date
-end
+  def initialize(message, key, date)
+    @message = message
+    @key = key
+    @date = date
+  end
+
+  def self.run(message, key, date)
+    decryption = Decryption.new(message, key, date)
+    decryption.message
+  end
 
 end
