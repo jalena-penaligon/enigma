20e147c3b583603ba48905bfa7ecb041b35687cf
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 09:09:10 -0700

    Passed test to find letter and index

--- lib/decryption.rb
+++ lib/decryption.rb
@@ -1,4 +1,6 @@
 class Decryption
+  include Shift
+  
   attr_reader :key, :date
   attr_accessor :message
 
@@ -11,6 +13,13 @@ class Decryption
   def self.run(message, key, date)
     decryption = Decryption.new(message, key, date)
     decryption.message
+  end
+
+  def find_letter_and_index
+    split_message = @message.split(//).to_enum
+    message_and_index = []
+    split_message.with_index { |letter,index| message_and_index << [letter, index] }
+    message_and_index
   end
 
 end
