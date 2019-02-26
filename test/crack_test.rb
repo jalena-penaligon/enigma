8bf5b9d06acafc1cff2a6109e1f3950630627db3
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-26 08:57:01 -0700

    Added test to split message and find index

--- test/crack_test.rb
+++ test/crack_test.rb
@@ -20,4 +20,20 @@ class CrackTest < Minitest::Test
     assert_equal 291018, crack.date
   end
 
+  def test_split_message_and_assign_index
+    crack = Crack.new("vjqtbeaweqihssi", 291018)
+    expected = [["v", 0], ["j", 1], ["q",2],
+                ["t", 3], ["b", 4], ["e", 5],
+                ["a", 6], ["w", 7], ["e", 8],
+                ["q", 9], ["i", 10], ["h", 11],
+                ["s", 12], ["s", 13], ["i", 14]]
+    assert_equal expected, crack.split_and_index
+  end
+
+  def test_find_a_letter
+    skip
+    crack = Crack.new("vjqtbeaweqihssi", 291018)
+
+    assert_equal "s", crack.a_key
+  end
 end
