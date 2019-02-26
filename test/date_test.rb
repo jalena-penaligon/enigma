e330bb6d0ee3d783ac22c9d8b6ad9ea70be7660e
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 17:22:20 -0700

    Updated date test to not hard code today's date

--- test/date_test.rb
+++ test/date_test.rb
@@ -5,14 +5,14 @@ class DateTest < Minitest::Test
   def test_you_can_create_todays_date
     enigma = Enigma.new
 
-    assert_equal "2019-02-22", enigma.create_date
+    assert_equal 10, enigma.create_date.length
   end
 
   def test_you_can_split_todays_date
     enigma = Enigma.new
     date = enigma.create_date
 
-    assert_equal ["2019", "02", "22"], enigma.date_split(date)
+    assert_equal 3, enigma.date_split(date).length
   end
 
   def test_year_is_last_2_characters
@@ -20,13 +20,14 @@ class DateTest < Minitest::Test
     date = enigma.create_date
     date = enigma.date_split(date)
 
-    assert_equal ["19", "02", "22"], enigma.date_slice(date)
+    assert_equal 4, date[0].length
+    assert_equal 2, enigma.date_slice(date)[0].length
   end
 
   def test_date_prints_DDMMYY
     enigma = Enigma.new
 
-    assert_equal "220219", enigma.todays_date
+    assert_equal 6, enigma.todays_date.length
   end
 
 end
