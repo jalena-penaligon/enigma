e000683700247bcd5fc76c1855d8ffc341aadeab
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-26 15:39:47 -0700

    Migrated create_key method to create module

---
+++ lib/create.rb
@@ -0,0 +1,29 @@
+require 'date'
+
+module Create
+
+  def create_date
+    date = Date.today.to_s
+  end
+
+  def date_split(date)
+    date.split(/-/)
+  end
+
+  def date_slice(date)
+    date[0].slice!(0,2)
+    return date
+  end
+
+  def todays_date
+    date = create_date
+    split_date = date_split(date)
+    final_date = date_slice(split_date)
+    final_date.reverse.join
+  end
+
+  def create_key
+    rand.to_s[2..6]
+  end
+  
+end
