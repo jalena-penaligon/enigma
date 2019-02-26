1dcb687555d5a84661eae7ff5a119bc7279230a4
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 17:00:07 -0700

    Initialized enigma and ran decrypt with file contents

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -3,3 +3,6 @@ require './test/test_helper'
 input = ARGV
 encrypted_file = File.open(input[0], "r")
 encryption = encrypted_file.read
+enigma = Enigma.new
+decrypted = enigma.decrypt(encryption, input[2], input[3])
+require 'pry'; binding.pry
