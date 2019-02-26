db332c68efeb76de330c70ad26fa179c1f3e7d7c
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-25 17:02:43 -0700

    Added functionality to write the message to a new file

--- lib/decrypt.rb
+++ lib/decrypt.rb
@@ -5,4 +5,7 @@ encrypted_file = File.open(input[0], "r")
 encryption = encrypted_file.read
 enigma = Enigma.new
 decrypted = enigma.decrypt(encryption, input[2], input[3])
-require 'pry'; binding.pry
+
+decrypted_file = File.open(input[1], "w")
+decrypted_file.write(decrypted[:decryption])
+decrypted_file.close
