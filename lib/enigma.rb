f12d6b90523fdc9e0cd62e2b282f9cbfae74073a
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-26 14:24:57 -0700

    Updated required files

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -1,10 +1,9 @@
-require './lib/date'
+require './lib/todays_date'
+require './lib/encryption'
+require './lib/decryption'
 
 class Enigma
   include TodaysDate
-
-  def initialize
-  end
 
   def encrypt(message, key = create_key, date = todays_date)
     encryption = Encryption.run(message, key, date)
@@ -15,9 +14,6 @@ class Enigma
     }
   end
 
-  # does this make sense to live here
-  # should I use this method as an arg instead?
-  # ie: def encrypt(message, key = rand.to_s[2..6], date = todays_date)
   def create_key
     rand.to_s[2..6]
   end
